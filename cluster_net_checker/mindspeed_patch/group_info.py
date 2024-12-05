85f7bb60989800522dcf32ad38e2d0732796634b
孔炳翔 <1120200577@qq.com>
2024-12-05 17:37:45 +0800

    update

--- cluster_net_checker/mindspeed_patch/group_info.py
+++ cluster_net_checker/mindspeed_patch/group_info.py
@@ -13,6 +13,22 @@ LOCAL_RANK_MAP = {
     "dp": ps.get_data_parallel_rank
 }
 
+DUMP_ARGS = [
+    "tensor_model_parallel_size",
+    "pipeline_model_parallel_size",
+    "data_parallel_size",
+    "context_parallel_size",
+    "expert_model_parallel_size",
+    "sequence_parallel",
+    "rank",
+    "world_size"
+]
+
+GROUP_NAME_KEY = "communication_group_name_map"
+GROUP_RANK_KEY = "communication_group_rank_map"
+DUMP_ARGS_KEY = "distributed_args"
+
+
 
 def get_golbal_rank(group):
     return torch.distributed.get_global_rank(group, torch.distributed.get_rank(group))
@@ -61,93 +77,17 @@ class GroupInfo:
     def get_args(self):
         args = vars(get_args())
 
-        dump_args = [
-            "tensor_model_parallel_size",
-            "pipeline_model_parallel_size",
-            "data_parallel_size",
-            "context_parallel_size",
-            "expert_model_parallel_size",
-            "sequence_parallel",
-            "rank",
-            "world_size"
-        ]
-
         res_list = []
-        for key in dump_args:
+        for key in DUMP_ARGS:
             res_list.append(str({key:args.get(key, "None")}))
         return res_list
 
     def get_group_info(self):
         res_list = []
         for item in self.group_map_list:
-            res_list.append(f"communication_group_name_map: {item}")
+            res_list.append(f"{GROUP_NAME_KEY}: {item}")
         for item in self.rank_map_list:
-            res_list.append(f"communication_group_rank_map: {item}")
+            res_list.append(f"{GROUP_RANK_KEY}: {item}")
         for item in self.args_list:
-            res_list.append(f"distributed_args: {item}")
+            res_list.append(f"{DUMP_ARGS_KEY}: {item}")
         return res_list
-
-
-
-"""
-class GroupInfo:
-    def __init__(self) -> None:
-        self.tp_group = ps.get_tensor_model_parallel_group()
-        self.pp_group = ps.get_pipeline_model_parallel_group()
-        self.ep_group = ps.get_expert_model_parallel_group()
-        self.dp_group = ps.get_data_parallel_group()
-
-        tp_rank = get_golbal_rank(self.tp_group)
-        pp_rank = get_golbal_rank(self.pp_group)
-        assert pp_rank == tp_rank
-        self.global_rank = tp_rank
-        self.group_dict = {
-            "pp": self.pp_group,
-            "tp": self.tp_group,
-            "ep": self.ep_group,
-            "dp": self.dp_group
-        }
-
-    def get_group_map(self):
-        res_list = []
-        for key, value in self.group_dict.items():
-            group_name = str(value._get_backend(torch.device("npu")).get_hccl_comm_name(self.global_rank))
-            res_list.append(
-                str({key: group_name})
-            )
-
-        res_str = ", ".join(res_list)
-
-        
-        return res_str
-
-    def get_rank_map(self):
-        res_list = []
-        for key, value in self.group_dict.items():
-            local_rank = LOCAL_RANK_MAP[key]()
-            group_name = str(value._get_backend(torch.device("npu")).get_hccl_comm_name(self.global_rank))
-            res_list.append(
-                str({group_name: str({local_rank: self.global_rank})})
-            )
-        res_str = ", ".join(res_list)
-        return res_str
-    
-    def get_args(self):
-        args = vars(get_args())
-
-        dump_args = [
-            "tensor_model_parallel_size",
-            "pipeline_model_parallel_size",
-            "data_parallel_size",
-            "context_parallel_size",
-            "expert_model_parallel_size",
-            "sequence_parallel",
-            "rank",
-            "world_size"
-        ]
-
-        res_dict = {}
-        for key in dump_args:
-            res_dict[key] = args.get(key, "None")
-        return str(res_dict)
-"""
