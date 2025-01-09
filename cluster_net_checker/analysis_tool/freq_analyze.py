e926753f0fae5a282aa5a81047a9ce5d328b992f
孔炳翔 <1120200577@qq.com>
2025-01-09 16:45:20 +0800

    add freq_ana

---
+++ cluster_net_checker/analysis_tool/freq_analyze.py
@@ -0,0 +1,176 @@
+import os
+
+import time
+from concurrent import futures
+from pathlib import Path
+import pandas as pd
+import sqlite3
+from functools import partial
+from collections import defaultdict
+
+QUERY_SQL = """
+SELECT
+    rdm.rankId,
+    freq.timestampNs,
+    freq.freq
+FROM
+    AICORE_FREQ freq
+CROSS
+    JOIN
+    RANK_DEVICE_MAP rdm
+"""
+
+DB_PATTERN = "ascend_pytorch_profiler_*.db"
+
+COMMON_FREQ = 1800
+
+FREE_FREQ = 800
+
+
+def connect_and_process_sql(database_path: Path, query_sql: str) -> object:
+    """
+    链接到指定的sql数据库路径 执行查询sql 返回查询结果
+    """
+    with sqlite3.connect(database_path) as conn:
+        df = pd.read_sql_query(query_sql, conn)
+    return df
+
+
+class MulitProcessor:
+    def __init__(self):
+        self._executor = futures.ProcessPoolExecutor(max_workers=os.cpu_count())
+
+    def __enter__(self):
+        if self._executor is None:
+            raise RuntimeError("executor is None")
+        return self
+    
+    def close(self):
+        if self._executor:
+            self._executor.shutdown(wait=True)
+            self._executor = None
+    
+    def launch(self, func, *args, **kwargs):
+        return self._executor.submit(func, *args, **kwargs).result()
+    
+    def map(self, func, *iterables, **kwargs):
+        partial_func = partial(func, **kwargs)
+        return list(self._executor.map(partial_func, *iterables))
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class DBLoader:
+    def __init__(self, input_path):
+        """
+        递归查找指定目录下所有匹配的数据库文件 并将每个数据库文件的查询结果合并到一个DataFrame中
+        输出一个包含所有数据的DataFrame
+        """
+        self.path = Path(input_path)
+        self.all_json_objects = pd.DataFrame([])
+
+        self.path_list = []
+        for db_file in self.path.rglob(DB_PATTERN):
+            if db_file.is_file():
+                self.path_list.append(db_file)
+        self.run()
+
+    @staticmethod
+    def _mapper_func(path_map):
+        json_obj = connect_and_process_sql(path_map, QUERY_SQL)
+        json_obj = pd.DataFrame(json_obj)
+        return json_obj
+    
+    def mapper_func(self, executor):
+        return executor.map(
+            self._mapper_func,
+            self.path_list,
+        )
+
+    def concat_db(self, db_list):
+        df_list = []
+        for json_obj in db_list:
+            if json_obj is not None:
+                df_list.append(json_obj)
+        
+        self.all_json_objects = pd.concat(df_list, ignore_index=True)
+
+    def run(self):
+        t1 = time.time()
+        print("loading data from db...")
+
+        with MulitProcessor() as executor:
+            mapper_res = self.mapper_func(executor)
+        
+        self.concat_db(mapper_res)
+        t2 = time.time()
+        print(f"loading cost time {t2 - t1}")
+
+
+def load_db(path):
+    db_loader = DBLoader(path)
+    return db_loader.all_json_objects
+
+
+class FreqProcessor:
+    def __init__(self, dfs):
+        self.rankid_arr = dfs["rankId"].values
+        self.time_arr = dfs["timestampNs"].values
+        self.freq_arr = dfs["freq"].values
+        self.run()
+
+    def parser_freq(self):
+        n = len(self.freq_arr)
+        dic = defaultdict(list)
+        freq_count = defaultdict(lambda: 0)
+
+        for idx in range(n):
+            freq = self.freq_arr[idx]
+            freq_count[freq] += 1
+            if freq not in dic[self.rankid_arr[idx]]:
+                dic[self.rankid_arr[idx]].append(freq)
+        
+        return dic, freq_count
+    
+    def run(self):
+        dic, _ = self.parser_freq()
+        error_ranks = []
+        free_ranks = []
+        for rank, value in dic.items():
+            if value == [COMMON_FREQ]:
+                pass
+            elif set(value) == set([COMMON_FREQ, FREE_FREQ]):
+                free_ranks.append(rank)
+            else:
+                error_ranks.append(rank)
+
+        print("=" * 50)
+        if len(free_ranks) > 0:
+            print(f"find ranks with free time, aic freq in {[COMMON_FREQ, FREE_FREQ]}: {free_ranks}")
+        else:
+            print("no rank found with free time")
+
+        print("=" * 50)
+        if len(error_ranks) > 0:    
+            print("find ranks with abnormal aic freq:")
+            for rank in error_ranks:
+                print(f"rank: {rank}, abnormal_freq: {dic[rank]}")
+        else:
+            print("no rank found with abnormal aic freq")
+
+
+def freq_process(dfs):
+    processor = FreqProcessor(dfs)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="freq analyze")
+    parser.add_argument('-d', '--path', type=str, required=True, help="profiling data path")
+    args = parser.parse_args()
+    
+    dfs = load_db(args.path)
+    freq_process(dfs)
+
