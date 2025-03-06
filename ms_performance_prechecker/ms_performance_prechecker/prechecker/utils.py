09d95fbbd2d77650b586938d260c609511f0b46c
王国伟 <leondgarse@163.com>
2025-03-06 05:03:16 +0000

    sync
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ms_performance_prechecker/ms_performance_prechecker/prechecker/utils.py
+++ ms_performance_prechecker/ms_performance_prechecker/prechecker/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 from collections import namedtuple
 
 CHECK_TYPES = namedtuple("CHECK_TYPES", ["basic", "deepseek"])("basic", "deepseek")
