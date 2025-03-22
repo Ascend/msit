46cf86e2e075014cbf96f3a19f803d00798f97f8
王国伟 <leondgarse@163.com>
2025-03-22 06:58:05 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ms_performance_prechecker/ms_performance_prechecker/__main__.py
+++ ms_performance_prechecker/ms_performance_prechecker/__main__.py
@@ -122,8 +122,8 @@ def run_precheck(
     for perchecker in percheckers:
         perchecker.precheck(env_save_path=save_env, mindie_service_path=mindie_service_path, **kwargs)
 
-    print_contents()
     if CHECKER_TYPES.basic in checkers or CHECKER_TYPES.all in checkers:
+        print_contents()
         logger.info("本工具提供的为经验建议，实际效果与具体的环境/场景有关，建议以实测为准")
 
 
