dd7b6b87b5d25b9fec36b2bb840e86072357fcae
王国伟 <leondgarse@163.com>
2025-04-22 08:55:18 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ms_performance_prechecker/ms_performance_prechecker/test/test_utils.py
+++ ms_performance_prechecker/ms_performance_prechecker/test/test_utils.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2025-2025 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import errno
 import unittest
 from unittest.mock import patch
@@ -11,8 +24,8 @@ from ms_performance_prechecker.prechecker.utils import (
     get_local_to_master_ip,
 )
 
-class TestUtils(unittest.TestCase):
 
+class TestUtils(unittest.TestCase):
     def test_str_ignore_case(self):
         self.assertEqual(str_ignore_case("Test_String"), "teststring")
         self.assertEqual(str_ignore_case("Another-Test"), "anothertest")
@@ -41,7 +54,3 @@ class TestUtils(unittest.TestCase):
     def test_get_local_to_master_ip(self, mock_socket):
         mock_socket.return_value.getsockname.return_value = ("192.168.1.1", 0)
         self.assertEqual(get_local_to_master_ip(), "192.168.1.1")
-
-
-if __name__ == "__main__":
-    unittest.main()
