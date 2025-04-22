832897dedf99db1741b3f1a75a59a9fbd68e4371
王国伟 <leondgarse@163.com>
2025-04-22 11:51:57 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ms_performance_prechecker/ms_performance_prechecker/prechecker/suggestions.py
+++ ms_performance_prechecker/ms_performance_prechecker/prechecker/suggestions.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-import typingli
+import typing
 from collections import namedtuple
 
 import yaml
