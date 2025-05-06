cde4224c5e2af90cf7eec52d9782f6f52f0a63b6
tucol <xujintao8@h-partners.com>
2025-05-06 14:49:27 +0800

    修复代码

--- modelevalstate/run_mindie.sh
+++ modelevalstate/run_mindie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PARENT_DIR=$(dirname$(realpath "$0"))
-export PYTHONPATH=PARENT_DIR
+PARENT_DIR=$(dirname $(realpath "$0"))
+export PYTHONPATH=$PARENT_DIR:$PYTHONPATH
 pkill -9 'mindie'
 ps -ef | grep 'python'
 ps -ef | grep 'mindie'
