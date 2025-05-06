a97b557f4e6d6aa8f61e277a80cceb152b58d86e
tucol <xujintao8@h-partners.com>
2025-05-06 14:42:14 +0800

    通过相对关系获取pythonpath

--- modelevalstate/run_mindie.sh
+++ modelevalstate/run_mindie.sh
@@ -1,4 +1,6 @@
-export PYTHONPATH=/data/deepseek/ModelEvalState:$PYTHONPATH #adapter
+#!/bin/bash
+PARENT_DIR=$(dirname$(realpath "$0"))
+export PYTHONPATH=PARENT_DIR
 pkill -9 'mindie'
 ps -ef | grep 'python'
 ps -ef | grep 'mindie'
