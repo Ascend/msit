07a42ee1aed207239cf3bd4d37c2b1a3e40ec012
梁斌明 <liangbinming@huawei.com>
2025-08-19 03:26:06 +0000

    update models/dancegrpo/fastvideo/models/flash_attn_no_pad.py.
    Signed-off-by: 梁斌明 <liangbinming@huawei.com>

--- models/dancegrpo/fastvideo/models/flash_attn_no_pad.py
+++ models/dancegrpo/fastvideo/models/flash_attn_no_pad.py
@@ -7,4 +7,5 @@ def flash_attn_no_pad(qkv,
                       dropout_p=0.0,
                       softmax_scale=None):
     # adapted from https://github.com/Dao-AILab/flash-attention/blob/13403e81157ba37ca525890f2f0f2137edf75311/flash_attn/flash_attention.py#L27
+    # 去引入的flash_attn功能，因为dancegrpo-flux并无引用，且NPU不支持该模块，当前去掉引入，后续移除文件
     pass
