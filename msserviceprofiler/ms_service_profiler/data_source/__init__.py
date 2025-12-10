dd1d6913c8416baae0c50f00cfbc2e6a5008c949
eveyin1 <qianyin2022@hotmail.com>
2025-12-10 14:18:41 +0800

    torch profiler
    Co-authored-by: eveyin1<qianyin2022@hotmail.com>

--- msserviceprofiler/ms_service_profiler/data_source/__init__.py
+++ msserviceprofiler/ms_service_profiler/data_source/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025-2025 Huawei Technologies Co., Ltd.
 
-__all__ = ["DBDataSource", "MsprofDataSource", "MsptiDataSource"]
+__all__ = ["DBDataSource", "MsprofDataSource", "MsptiDataSource", "TorchProfilerDataSource"]
 from .db_data_source import DBDataSource
 from .msprof_data_source import MsprofDataSource
 from .mspti_data_source import MsptiDataSource
+from .torch_profiler_data_source import TorchProfilerDataSource
